ee56c5df180ef0d65268f53627cc4a193e411eff
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-03 16:35:03 +0100

    update

---
+++ Facebook-udemy/udemy/src/components/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='login-form'>
+      <h2>Login</h2>
+
+      <form action=""></form>  
+    </div>
+  )
+}
+
+export default login

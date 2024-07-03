e4d59658c1be76286651ff6d092298a055ddd5ba
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-03 16:19:08 +0100

    register user

--- Facebook-udemy/udemy/src/facebook.jsx
+++ Facebook-udemy/udemy/src/facebook.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import { useState } from "react";
 
 export default function facebook() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  function registerUser(event){
+    event.preventDefault();
+
+    const user =JSON.parse(localStorage.getItem("users") || "[]")
+    const newUser ={
+     username:username,
+     password:password
+       
+    }
+users.push(newUser)
+
+    localStorage.setItem("users",JSON.stringify(newUser))
+  }
 
   return (
     <form className="form-control">
@@ -15,14 +29,16 @@ export default function facebook() {
           className="username"
           placeholder="Enter Username"
           value={username}
+          onChange={(e)=>setUsername(e.target.value)}
         />
         <input
           type="text"
           className="password"
           placeholder="Enter Password"
           value={password}
+          onChange={(e)=>setPassword(e.target.value)}
         />
-        <input type="submit" />
+        <input type="submit" onSubmit={registerUser}/>
       </div>
     </form>
   );

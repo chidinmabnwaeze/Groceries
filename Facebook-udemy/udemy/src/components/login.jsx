e3d3e446ab055b89fcaa88b6a833fa92a43a6fff
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-03 16:55:08 +0100

    form

--- Facebook-udemy/udemy/src/components/login.jsx
+++ Facebook-udemy/udemy/src/components/login.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const login = () => {
-
-    const [username,setUsername]=useState()
-    const [password, setPassword]= useState()
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
 
   return (
-    <div className='login-form'>
+    <div className="login-form">
       <h2>Login</h2>
 
       <form action="">
-        <input type="text" 
-        placeholder='Enter username'
-        value={username}
-        onChange={(e)=>setUsername(e.target.value)}
-        
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        </form>  
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;

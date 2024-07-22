71f70bace28390e35e1e702b6ada3f6b73c73d76
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-22 23:51:19 +0100

    meals app

---
+++ Meals_app/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <h1>React Meals</h1>
+        <a href="#">Home</a>
+        
+    </div>
+  )
+}
+
+export default navbar

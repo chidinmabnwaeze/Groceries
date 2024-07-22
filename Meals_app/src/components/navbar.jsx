d1737a41c87e16cc157ae68e8ab2de59948381e8
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-23 00:00:32 +0100

    meala

--- Meals_app/src/components/navbar.jsx
+++ Meals_app/src/components/navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const navbar = () => {
   return (
     <div>
-        <h1>React Meals</h1>
-        <a href="#">Home</a>
-        
+        <h1 style={{backgroundColor:'brown'}}>React Meals</h1>
+        <a  href="#">Home</a>
+
     </div>
   )
 }

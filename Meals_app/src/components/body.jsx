47e168a1810a1c75b87b77ff0c364692af14559d
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-23 02:22:08 +0100

    update

--- Meals_app/src/components/body.jsx
+++ Meals_app/src/components/body.jsx
@@ -1,25 +1,33 @@
-import React, {useEffect} from "react";
-import axios from 'axios'
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export default function body() {
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        console.log(res.data);
 
-    axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
-    .then(res =>{
+        setItems(res.data.meals);
+      })
+      .catch((err) => {});
+  }, []);
 
-        console.log(res.data)
-    }).catch(err =>{
-
-    })
-  
-    }, []);
+  const itemsList = items.map((obj) => {
+    return (
+      <div>
+        <p>{obj.strMeal}</p>
+        <p>{obj.strMealThumb}</p>
+      </div>
+    );
+  });
 
   return (
     <div>
-      <h1>body component</h1>
-
-      <button className="btn btn-primary">A bootsrap button</button>
+      <div className="row"></div>
     </div>
   );
 }

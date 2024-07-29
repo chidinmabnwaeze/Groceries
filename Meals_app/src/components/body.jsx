77a944fabcae2a23e33c266fb5b9c48c3218e97b
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-29 23:01:46 +0100

    css

--- Meals_app/src/components/body.jsx
+++ Meals_app/src/components/body.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "tailwindcss/tailwind.css"
 // import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export default function body() {

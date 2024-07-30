f7311547cf171fec47e474db86e646af7857411b
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-30 15:29:23 +0100

    installed tailwindcss

--- Meals_app/src/components/navbar.jsx
+++ Meals_app/src/components/navbar.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 const navbar = () => {
   return (
+    <div>
+
     <div style={{backgroundColor:'brown'}}>
         <h1>React Meals</h1>
         <a style={{textDecoration: 'none', color: 'white'}} href="#">Home</a>
         <a style={{textDecoration: 'none', color: 'white'}} href="#">Order</a>
         <a style={{textDecoration: 'none', color: 'white'}} href="#">Contact Us</a>
 
+    </div>
+    <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
     </div>
   )
 }

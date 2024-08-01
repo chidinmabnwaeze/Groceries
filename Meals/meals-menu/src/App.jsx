2503797ab207fa2209336055b0137291bf439897
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-01 23:23:39 +0100

    api

---
+++ Meals/meals-menu/src/App.jsx
@@ -0,0 +1,21 @@
+import { data } from 'autoprefixer'
+import './App.css'
+
+function App() {
+
+  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+  .then((res)=> res.json())
+  .then((data)=> {
+    console.log(data);
+  })
+
+  return (
+    <>
+     <h1 className='text-4xl text-red-500'>Meals</h1>
+
+     
+    </>
+  )
+}
+
+export default App

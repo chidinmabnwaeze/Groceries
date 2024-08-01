5f7597e49beaf76cb3a45a92a27e972041877b74
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-01 23:49:49 +0100

    api

--- Meals/meals-menu/src/App.jsx
+++ Meals/meals-menu/src/App.jsx
@@ -1,14 +1,19 @@
 import { data } from 'autoprefixer'
+import axios from 'axios'
 import './App.css'
 
 function App() {
 
-  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-  .then((res)=> res.json())
-  .then((data)=> {
-    console.log(data);
-  })
+  // fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+  // .then((res)=> res.json())
+  // .then((data)=> {
+  //   console.log(data);
+  // })
 
+  axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+  .then((res)=>{
+    console.log(res.data);
+  })
   return (
     <>
      <h1 className='text-4xl text-purple-500'>Meals</h1>

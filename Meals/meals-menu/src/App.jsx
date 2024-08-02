98bfe434abfd96ea437ed452c89ea076a4e44820
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-02 01:44:27 +0100

    loading

--- Meals/meals-menu/src/App.jsx
+++ Meals/meals-menu/src/App.jsx
@@ -11,16 +11,59 @@ function App() {
   //   console.log(data);
   // })
 
-  const [recipe , setRecipe] = useState("")
+  // const [recipe , setRecipe] = useState("")
+  // axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+  // .then((res)=>{
+  //   console.log(res.data);
+  // })
 
-  axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-  .then((res)=>{
-    console.log(res.data);
-  })
+  const [name , setName]= useState("")
+  const [predictedAge ,setPredictedAge] = useState("")
+  const [isLoading , setIsLoading] = useState(true)
+
+  const fetchNameData = () =>{
+    axios.get(`https://api.agify.io/?name=${name}`)
+    .then((res) =>{
+      setPredictedAge(res.data)
+      try{
+
+      }
+      finally{
+        setIsLoading(false)
+      }
+    })
+
+    setInterval(()=>{
+      (async () => await fetchNameData())();
+    },2000)
+  }
+
+
   return (
     <>
-     <h1 className='text-4xl text-purple-500'>Meals</h1>
+     <h1 className='text-4xl text-purple-500'>Predict Your Age</h1>
 
+     <input type="text" placeholder='E.g name' 
+     value={name}
+     onChange={(event) =>{setName(event.target.value)}}
+     />
+     <button onClick={fetchNameData}>Predict Age</button>
+     
+     {isLoading && <p>...Loading data</p>}
+     {!isLoading && 
+     <table>
+     <tr>
+      <th>Name</th>
+      <th>Predicted Age</th>
+      <th>Count</th>
+     </tr>
+     <tr>
+      <td>{predictedAge.name}</td>
+      <td>{predictedAge.age}</td>
+      <td>{predictedAge.count}</td>
+     </tr>
+     </table>
+      }
      
     </>
   )

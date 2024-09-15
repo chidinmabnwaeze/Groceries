f1d98354c38c20da4bf53ec77d9da544eacbab99
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-09-15 22:19:21 +0100

    message

--- Meals/meals-menu/src/App.jsx
+++ Meals/meals-menu/src/App.jsx
@@ -1,10 +1,9 @@
-import { data } from 'autoprefixer'
-import axios from 'axios'
-import React, {useState} from 'react'
-import './App.css'
+import { data } from "autoprefixer";
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   // fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
   // .then((res)=> res.json())
   // .then((data)=> {
@@ -17,56 +16,55 @@ function App() {
   //   console.log(res.data);
   // })
 
-  const [name , setName]= useState("")
-  const [predictedAge ,setPredictedAge] = useState("")
-  const [isLoading , setIsLoading] = useState(true)
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchNameData = () =>{
-    axios.get(`https://api.agify.io/?name=${name}`)
-    .then((res) =>{
-      setPredictedAge(res.data)
-      try{
-
+  const fetchNameData = () => {
+    axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
+      try {
+      } finally {
+        setIsLoading(false);
       }
-      finally{
-        setIsLoading(false)
-      }
-    })
+    });
 
-    setInterval(()=>{
+    setInterval(() => {
       (async () => await fetchNameData())();
-    },2000)
-  }
-
+    }, 2000);
+  };
 
   return (
     <>
-     <h1 className='text-4xl text-purple-500'>Predict Your Age</h1>
+      <h1 className="text-4xl text-purple-500">Predict Your Age</h1>
 
-     <input type="text" placeholder='E.g name' 
-     value={name}
-     onChange={(event) =>{setName(event.target.value)}}
-     />
-     <button onClick={fetchNameData}>Predict Age</button>
-     
-     {isLoading && <p>...Loading data</p>}
-     {!isLoading && 
-     <table>
-     <tr>
-      <th>Name</th>
-      <th>Predicted Age</th>
-      <th>Counts</th>
-     </tr>
-     <tr>
-      <td>{predictedAge.name}</td>
-      <td>{predictedAge.age}</td>
-      <td>{predictedAge.count}</td>
-     </tr>
-     </table>
-      }
-     
+      <input
+        type="text"
+        placeholder="E.g name"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchNameData}>Predict Age</button>
+
+      {isLoading && <p>...Loading data</p>}
+      {!isLoading && (
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Predicted Age</th>
+            <th>Counts</th>
+          </tr>
+          <tr>
+            <td>{predictedAge.name}</td>
+            <td>{predictedAge.age}</td>
+            <td>{predictedAge.count}</td>
+          </tr>
+        </table>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

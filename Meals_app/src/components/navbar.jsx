95a1e63bf14fbe73950cf4d37274b31963ccbd13
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-11-24 00:00:18 +0100

    push

--- Meals_app/src/components/navbar.jsx
+++ Meals_app/src/components/navbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const navbar = () => {
   return (
     <div>
-
-    <div style={{backgroundColor:'brown'}}>
+      <div style={{ backgroundColor: "brown" }}>
         <h1>React Meals</h1>
-        <a style={{textDecoration: 'none', color: 'white'}} href="#">Home</a>
-        <a style={{textDecoration: 'none', color: 'white'}} href="#">Order</a>
-        <a style={{textDecoration: 'none', color: 'white'}} href="#">Contact Us</a>
-
+        <a style={{ textDecoration: "none", color: "white" }} href="#">
+          Home
+        </a>
+        <a style={{ textDecoration: "none", color: "white" }} href="#">
+          Order
+        </a>
+        <a style={{ textDecoration: "none", color: "white" }} href="#">
+          Contact Us
+        </a>
+      </div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
